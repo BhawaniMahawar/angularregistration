a9f7e375e9e32daf159034dcf1bb7c9cd0b25578
Kundan Singh <kundan.singh979@gmail.com>
2016-07-13 11:37:35 +0530

    Added navigation feature

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,11 +1,11 @@
-myApp.controller('RegistrationController', ['$scope', function($scope) {
-	$scope.message = "Welcome to Kundan's App";
+myApp.controller('RegistrationController', ['$scope', function ($scope) {
+	$scope.message = "";
 
-	$scope.login	=	function() {
-		$scope.message	=	"Welcome " + $scope.user.email;
+	$scope.login = function () {
+		$scope.message = "Welcome " + $scope.user.email;
 	};
 
-	$scope.register	=	function() {
-		$scope.message	=	"Welcome " + $scope.user.firstname;
- 	};
+	$scope.register = function () {
+		$scope.message = "Welcome " + $scope.user.firstname;
+	};
 }]);
